44f482d02b66c68ad93f15764dd6fa00d434f781
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-08-25 13:10:57 +0300

    feat: finish general info and word

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
-  NAME: "Astro Nano",
-  EMAIL: "markhorn.dev@gmail.com",
+  NAME: "Dagim G. Astatkie",
+  EMAIL: "dagim.astatkie@gmail.com",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
@@ -29,16 +29,16 @@ export const PROJECTS: Metadata = {
 };
 
 export const SOCIALS: Socials = [
-  { 
-    NAME: "twitter-x",
-    HREF: "https://twitter.com/markhorn_dev",
+  {
+    NAME: "telegram",
+    HREF: "https://t.me/aion_ue",
   },
   { 
     NAME: "github",
-    HREF: "https://github.com/markhorn-dev"
+    HREF: "https://github.com/dagimg-dot"
   },
   { 
     NAME: "linkedin",
-    HREF: "https://www.linkedin.com/in/markhorn-dev",
+    HREF: "https://www.linkedin.com/in/dagim-astatkie-1743a1239",
   }
 ];
---
+++ src/content/work/poshak.md
@@ -0,0 +1,8 @@
+---
+company: "Poshak Consultancy Services PLC"
+role: "Mobile Application Developer Intern"
+dateStart: "Oct 2022"
+dateEnd: "Jan 2023"
+---
+
+Poshak Consultancy Services is a specialized IT consultancy firm focused on developing tailored ERP (Enterprise Resource Planning) systems for garment factories in Ethiopia and India. During my tenure, I worked on fixing bugs in a mobile app, which helped streamline the release process, ensuring adherence to project deadlines. Additionally, I contributed to enhancing user experience by addressing critical issues in the mobile app prior to its launch and worked specifically on a key mobile application for inventory management, collaborating with designers to improve the user interface and overall experience.
